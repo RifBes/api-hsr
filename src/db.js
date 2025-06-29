250d0627c89af9f0fddeba60b0265767865f1a95
Arina B <arina-bespalova-2018@mail.ru>
2025-06-29 15:03:04 +0300

    fix

--- src/db.js
+++ src/db.js
@@ -3,17 +3,7 @@ import "dotenv/config";
 import pkg from "pg";
 const { Pool } = pkg;
 
-// export const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_DATABASE,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-// });
-
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: { rejectUnauthorized: false },
 });
